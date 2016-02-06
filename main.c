e0fdde282e59020feb0f69d6165611d4ed7b8499
David <DavidHong@outlook.fr>
2016-02-06 16:09:19 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+#include "morpion.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+  morpion_t partie = creer_partie();
+  int vainqueur = VIDE;
+  while(vainqueur == VIDE && i < 9)
+  {
+    vainqueur = tour(&partie);
+    afficher_morpion(partie);
+    changer_joueur(&partie);
+    i++;
+  }
+
+if(vainqueur == ROND)
+  printf("LE JOUEUR ROND A GAGNÉ\n");
+else if(vainqueur == CROIX)
+  printf("LE JOUEUR CROIX A GAGNÉ\n");
+else
+  printf("PErSONNE N'A GAGNE\n");
+
+return EXIT_SUCCESS;
+
+}
