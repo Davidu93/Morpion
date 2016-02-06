a2319a52057d3faae21a136dfa5b71084bf33078
David <DavidHong@outlook.fr>
2016-02-06 15:37:16 +0100

    Create Morpion.h

---
+++ Morpion.h
@@ -0,0 +1,19 @@
+#ifndef _MORPION_H
+#define _MORPION_H
+
+#define ROND 0
+#define CROIX 1
+#define VIDE 2
+
+typedef struct{
+  int plateau[3][3];
+  int joueur;
+}morpion_t;
+
+morpion_t creer_partie();
+void afficher_plateau(morpion_t plateau);
+void changer_joueur(morpion_t * plateau);
+int tour(morpion_t * m);
+int game_over(morpion_t m);
+
+#endif
